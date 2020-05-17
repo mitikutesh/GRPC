e17582f8b0aa62f3d4ffe62b3d30d63c6f8d3abd
Mitiku Geleta <mitikutesh@gmail.com>
2020-05-17 11:44:02 +0300

    -add nuget packages 1- google.protopuf 2-grpc.net.client 3-grpc.tools

---
+++ Grpc.Client/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grpc.Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private readonly ILogger<MessageController> _logger;
+
+        public MessageController(ILogger<MessageController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
